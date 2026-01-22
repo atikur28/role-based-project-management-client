0f0932f898866595c7f86587745815bab8a93931
atikur28 <atikur45683@gmail.com>
2026-01-22 11:34:15 +0600

    Timeout implemented on Toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+
 interface ToastProps {
   message: string;
   type?: "success" | "error";
 }
 
 const Toast = ({ message, type = "success" }: ToastProps) => {
+  const [visible, setVisible] = useState(true);
+
+  const duration = 4000;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!visible) return null;
+
   const bgColor = type === "success" ? "bg-green-500" : "bg-red-500";
+
   return (
     <div
       className={`${bgColor} text-white p-3 rounded shadow fixed top-4 right-4 z-50`}
